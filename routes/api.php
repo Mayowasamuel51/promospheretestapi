cdfca2ae11d6e0a6149c580195d9e3c80455e5ca
mayowasamuel51 <fpasamuelmayowa51@gmail.com>
2023-10-05 13:35:55 +0000

    api

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::get('/views',function(){
+    return [
+        "name",
+    ];
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
