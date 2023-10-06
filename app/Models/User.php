59cba72677d50c164da32c87174d5650a5ca497d
mayowasamuel51 <fpasamuelmayowa51@gmail.com>
2023-10-06 07:26:14 +0000

    new update

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,8 +56,11 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
+    // protected $casts = [
+    //     'email_verified_at' => 'datetime',
+    //     'password' => 'hashed',
+    // ];
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 
